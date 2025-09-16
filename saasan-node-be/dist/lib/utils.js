6256c7b9ebe52cf410903dfc194adfb5e4ac0101
Pukar Chhatkuli <dev.pukar.chhatkuli@gmail.com>
2025-09-16 16:33:51 +0545

    remove dist

--- saasan-node-be/dist/lib/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUUID = generateUUID;
-const uuid_1 = require("uuid");
-function generateUUID() {
-    return (0, uuid_1.v4)();
-}
